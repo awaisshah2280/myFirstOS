8b9bfff8fdc4d54e1816562d73b18c53f7af69fc
awaisshah <m.awaisshah228@gmail.com>
2021-04-14 13:08:49 +0500

    l

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,10 +2,12 @@
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("****  ****  ****   ******  **** \n");
-    print_str("*     *     *      *       *   \n");
-    print_str("****  ****  ****   *       ****\n");
+    print_set_color(PRINT_COLOR_LIGHT_CYAN, PRINT_COLOR_BLACK);
+print_str("//////////////////////////////////////////////\n");
+    print_str("**  **  **   **   ** \n");
+    print_str("*     *     *      *        *   \n");
+    print_str("**  **  **   *        **\n");
     print_str("   *  *     *      *          *\n");
-    print_str("****  ****  ****   *****   ****\n");
+    print_str("**  **  **   **   **\n");
+    print_str("//////////////////////////////////////////////\n");
 }
