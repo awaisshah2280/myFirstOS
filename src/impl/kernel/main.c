33ca978f6792e317c58b23458068cee4c220f059
awaisshah <m.awaisshah228@gmail.com>
2021-04-14 13:03:20 +0500

    printing seecs usiing asteriks

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,9 +3,9 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("****  ****  ****   ******  **** ");
-    print_str("*     *     *      *       *   ");
-    print_str("****  ****  ****   *       ****");
-    print_str("   *  *     *      *          *");
-    print_str("****  ****  ****   *****   ****");
+    print_str("****  ****  ****   ******  **** \n");
+    print_str("*     *     *      *       *   \n");
+    print_str("****  ****  ****   *       ****\n");
+    print_str("   *  *     *      *          *\n");
+    print_str("****  ****  ****   *****   ****\n");
 }
