dfd3d37aa683fd9391d6697ce9af8f6dc3d9db1b
awaisshah <m.awaisshah228@gmail.com>
2021-04-14 12:21:24 +0500

    printing logo

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,5 +3,9 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("Welcome to our 64-bit kernel!");
+    print_str("****  ****  ****   ******  **** ");
+    print_str("*     *     *      *       *   ");
+    print_str("****  ****  ****   *       ****");
+    print_str("   *  *     *      *          *");
+    print_str("****  ****  ****   *****   ****");
 }
